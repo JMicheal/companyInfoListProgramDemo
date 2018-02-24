9207adbd4f386e1f0b8c02cb91bb55ca06bd02bb
jiangfan <lannikeng@.com>
2018-02-24 11:26:07 +0800

    Initial Commit

---
+++ companyInfoListProgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  companyInfoListProgram
+//
+//  Created by dixintong_JF on 2018/2/24.
+//  Copyright © 2018年 dixintong_JF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
